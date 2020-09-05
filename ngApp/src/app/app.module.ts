80a6d615de086fe0e43df84023e179e6b74659ea
Paulstar200 <vitalispaul48@live.com>
2020-09-05 19:48:02 +0300

    Add components

--- ngApp/src/app/app.module.ts
+++ ngApp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterComponent } from './register/register.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    RegisterComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
