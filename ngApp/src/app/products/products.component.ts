e5712dc72a7b68b4325c7845b8bab233a8af228a
Paulstar200 <vitalispaul48@live.com>
2020-09-06 12:58:21 +0300

    Add check for unauthorized token

--- ngApp/src/app/products/products.component.ts
+++ ngApp/src/app/products/products.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService  } from '../products.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import  { Router } from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -9,13 +11,20 @@ import { ProductsService  } from '../products.service';
 export class ProductsComponent implements OnInit {
 
   products = []
-  constructor(private _productsService: ProductsService) { }
+  constructor(private _productsService: ProductsService,
+              private _router: Router) { }
 
   ngOnInit(): void {
     this._productsService.getProducts()
     .subscribe(
       res => this.products = res.Products,
-      err => console.log('Error:', err)
+      err => {
+        if(err instanceof HttpErrorResponse) {
+          if (err.status === 401  || err.status === 422){
+            this._router.navigate(['/login'])
+          }
+        }
+      }
     )
   }
 
