a4218e4ee32ba7b0bd970e9f0e1b62b333792d6f
Paulstar200 <vitalispaul48@live.com>
2020-09-06 11:46:35 +0300

    Add login UI values

--- ngApp/src/app/auth.service.ts
+++ ngApp/src/app/auth.service.ts
@@ -7,9 +7,15 @@ import { HttpClient } from '@angular/common/http';
 export class AuthService {
 
   private _registerUrl = "https://storemanagerapi2.herokuapp.com/api/v2/auth/signup"
+  private _loginUrl = "https://storemanagerapi2.herokuapp.com/api/v2/auth/login"
   constructor(private http: HttpClient) { }
 
   registerUser(user) {
     return this.http.post<any>(this._registerUrl, user)
+  }
+
+  loginUser(user) {
+    return this.http.post<any>(this._loginUrl, user)
+    // returns  observable
   }
 }
